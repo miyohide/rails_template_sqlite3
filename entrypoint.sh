75707c95a838b850696ec5e666576e3ad0db8402
Hidenori Miyoshi <miyohide@gmail.com>
2021-07-22 17:35:32 +0900

    delete ssh command

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,5 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
-/usr/sbin/sshd
-
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
