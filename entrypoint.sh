07154bd19628ceae776ab9b0ce45c3f388331eb1
Hidenori Miyoshi <miyohide@gmail.com>
2021-07-23 09:33:29 +0900

    run migration

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,10 @@ service ssh start
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
+# 起動時にマイグレーションを実行
+if [[ $RAILS_ENV = 'production' ]]; then
+  bin/rails db:migrate
+fi
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
