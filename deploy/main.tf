990f33993b52c1098022b992194a259d8ae92f23
Hidenori Miyoshi <miyohide@gmail.com>
2021-07-28 21:16:00 +0900

    add tag

--- deploy/main.tf
+++ deploy/main.tf
@@ -17,6 +17,11 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "japaneast"
+
+  tags = {
+    "Environment" = "Terraform Getting Started"
+    "Team" = "DevOps"
+  }
 }
 
 # Create a virtual network
