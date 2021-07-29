0c7cd7a7a5578c880dacd2fd5fd72df9a3b69c8b
Hidenori Miyoshi <miyohide@gmail.com>
2021-07-29 20:50:14 +0900

    create container registory

--- deploy/main.tf
+++ deploy/main.tf
@@ -15,19 +15,15 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
+  name     = "rg-railsapp"
   location = "japaneast"
-
-  tags = {
-    "Environment" = "Terraform Getting Started"
-    "Team" = "DevOps"
-  }
 }
 
-# Create a virtual network
-resource "azurerm_virtual_network" "vnet" {
-    name                = "myTFVnet"
-    address_space       = ["10.0.0.0/16"]
-    location            = "japaneast"
-    resource_group_name = azurerm_resource_group.rg.name
+# Create an Azure Container Registory
+resource "azurerm_container_registry" "acr" {
+  name                     = "crmiyohiderailsapp"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  sku                      = "Basic"
+  admin_enabled            = true
 }
