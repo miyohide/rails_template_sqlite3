93eaa7f3a1fdea56e5d917625c7d91077dc4ac00
Hidenori Miyoshi <miyohide@gmail.com>
2021-07-31 20:34:43 +0900

    create app service with nginx image

--- deploy/main.tf
+++ deploy/main.tf
@@ -41,3 +41,18 @@ resource "azurerm_app_service_plan" "appplan" {
     size = "B1"
   }
 }
+
+resource "azurerm_app_service" "appservice" {
+  name = "app-miyohiderails"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.appplan.id
+
+  site_config {
+    linux_fx_version = "DOCKER|nginx:latest"
+  }
+
+  app_settings = {
+    "DOCKER_REGISTRY_SERVER_URL" = "https://index.docker.io/v1"
+  }
+}
