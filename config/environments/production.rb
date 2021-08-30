3735ef2b609d2e2bbd698f43b25999652728e0c0
Hidenori Miyoshi <miyohide@gmail.com>
2021-08-30 20:52:22 +0900

    delete test config in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,7 +104,6 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.hosts << ".azurewebsites.net"
-  config.hosts << "localhost"  ## for test
 
   config.paths['log'] = "/mylogs/logs/production.log"
 end
