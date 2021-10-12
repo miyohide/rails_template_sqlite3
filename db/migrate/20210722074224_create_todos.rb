bcd681b6238ce5075dc93b2a41a1be3423e63074
Hidenori Miyoshi <miyohide@gmail.com>
2021-10-12 20:28:12 +0900

    add limit and null option.

--- db/migrate/20210722074224_create_todos.rb
+++ db/migrate/20210722074224_create_todos.rb
@@ -1,8 +1,8 @@
 class CreateTodos < ActiveRecord::Migration[6.1]
   def change
     create_table :todos do |t|
-      t.string :title
-      t.text :body
+      t.string :title, limit: 100, null: false
+      t.text :body, limit: 500, null: false
 
       t.timestamps
     end
