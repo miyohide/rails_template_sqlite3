1e821e1619417bf345e6e59fb794144616bada64
Hidenori Miyoshi <miyohide@gmail.com>
2021-11-28 21:06:49 +0900

    run db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_22_074224) do
+ActiveRecord::Schema.define(version: 2021_11_28_120348) do
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
+  end
 
   create_table "todos", force: :cascade do |t|
     t.string "title", limit: 100, null: false
