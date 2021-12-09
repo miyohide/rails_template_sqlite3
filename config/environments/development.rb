4685260e34ae9c35140be868baf95a8948d01f0f
Hidenori Miyoshi <miyohide@gmail.com>
2021-12-09 20:40:28 +0900

    setup redis for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,4 +65,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.session_store :redis_store,
+    servers: ["redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/#{ENV['REDIS_DB']}/session"],
+    :key => '_rails_template_sqlite3_session',
+    expire_after: 10.minutes
 end
