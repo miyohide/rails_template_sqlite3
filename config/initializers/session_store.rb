d4eafc65db5fef282580cd9b8b48436584d31cf0
Hidenori Miyoshi <miyohide@gmail.com>
2021-12-09 20:40:14 +0900

    disabled session store config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
-Rails.application.config.session_store :redis_store,
-  servers: %w(redis://redis:6379/0/session),
-  :key => '_rails_template_sqlite3_session',
-  expire_after: 10.minutes
+# Rails.application.config.session_store :redis_store,
+#   servers: %w(redis://redis:6379/0/session),
+#   :key => '_rails_template_sqlite3_session',
+#   expire_after: 10.minutes
